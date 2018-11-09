3343d67a9c40ddcf2a6b5d561596dd01e86bf321
Brad Nelson <flagxor@gmail.com>
2018-11-08 23:20:32 -0800

    Improve startup.

--- voiceforth.js
+++ voiceforth.js
@@ -4,21 +4,24 @@ const child_process = require('child_process');
 const http = require('http');
 const process = require('process');
 
-const gforth = child_process.spawn('gforth');
-
 var pendingOutput = '';
+var gforth;
 
-gforth.stdout.on('data', (data) => {
-  pendingOutput += data.toString();
-});
-
-gforth.stderr.on('data', (data) => {
-  pendingOutput += data.toString();
-});
-
-gforth.on('exit', (code) => {
-  process.exit(code);
-});
+function launchGforth() {
+  if (gforth !== undefined) {
+    gforth.kill('SIGQUIT');
+  }
+  gforth = child_process.spawn('gforth', ['-e', 'quit']);
+  gforth.stdout.on('data', (data) => {
+    pendingOutput += data.toString();
+  });
+  gforth.stderr.on('data', (data) => {
+    pendingOutput += data.toString();
+  });
+  gforth.on('exit', (code) => {
+    process.exit(code);
+  });
+}
 
 function prepReply(text) {
   const reply = {
@@ -58,12 +61,17 @@ const server = http.createServer((request, response) => {
   request.on('end', function() {
     try {
       const req = JSON.parse(requestBody);
-      gforth.stdin.write(req.inputs[0].rawInputs[0].query + '\n');
+      const query = req.inputs[0].rawInputs[0].query;
+      if (query.toLowerCase() == 'talk to voice forth') {
+        launchGforth();
+      } else {
+        gforth.stdin.write(req.inputs[0].rawInputs[0].query + '\n');
+        pendingOutput = '';  // Something better?
+      }
     } catch (e) {
       response.end();
       return;
     }
-    pendingOutput = '';  // Something better?
     setTimeout(() => {
       response.writeHead(200, {'Content-Type': 'text/html'});
       response.write(prepReply(pendingOutput));
