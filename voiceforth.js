3eb333dccc74e86330bb23c5fec649828d2ab12d
Brad Nelson <flagxor@gmail.com>
2018-11-09 11:34:32 -0800

    Adding passwd.

--- voiceforth.js
+++ voiceforth.js
@@ -2,8 +2,10 @@
 
 const child_process = require('child_process');
 const http = require('http');
+const fs = require('fs');
 const process = require('process');
 
+var expected_passwd = fs.readFile('passwd').trim();
 var pendingOutput = '';
 var gforth;
 
@@ -23,9 +25,13 @@ function launchGforth() {
   });
 }
 
-function prepReply(text) {
+function prepReply(passwd, text, tts) {
+  if (tts === undefined) {
+    tts = text;
+  }
   const reply = {
     conversationToken: '',
+    userStorage: passwd,
     expectUserResponse: true,
     expectedInputs: [
       {
@@ -34,7 +40,7 @@ function prepReply(text) {
             items: [
               {
                 simpleResponse: {
-                  textToSpeech: text,
+                  textToSpeech: tts,
                   displayText: text,
                 },
               },
@@ -62,19 +68,35 @@ const server = http.createServer((request, response) => {
     try {
       const req = JSON.parse(requestBody);
       const query = req.inputs[0].rawInputs[0].query;
-      if (query.toLowerCase() == 'talk to voice forth') {
-        launchGforth();
-      } else {
-        gforth.stdin.write(query + '\n');
-        pendingOutput = '';  // Something better?
+      if (req.userStorage != expected_pass) {
+        if (query.toLowerCase() == expected_pass) {
+          response.write(prepReply(expected_pass, 'ok');
+          response.end();
+          return;
+        }
+        response.write(prepReply('', "What's the password?"));
+        response.end();
+        return;
       }
+      if (gforth === undefined) {
+        launchGforth();
+      }
+      if (query.toLowerCase() == 'talk to voice forth') {
+        response.write(prepReply(req.userStorage, 'ok');
+        response.end();
+        return;
+      }
+      gforth.stdin.write(query + '\n');
+      pendingOutput = '';  // Something better?
       setTimeout(() => {
         response.writeHead(200, {'Content-Type': 'text/html'});
         // Strip input if the same.
         if (pendingOutput.substr(0, query.length) == query) {
           pendingOutput = pendingOutput.substr(query.length);
         }
-        response.write(prepReply(pendingOutput));
+        var tts = pendingOutput.split('\n')[0];
+        tts = tts.replace(/:[0-9]+:/, '');
+        response.write(prepReply(req.userStorage, pendingOutput, tts));
         pending = '';
         response.end();
       }, 100);
